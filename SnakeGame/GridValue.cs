245e77e485310fbff172f4841c2e2253f06c3bd6
julia0306 <123451628+julia0306@users.noreply.github.com>
2024-03-03 11:36:51 +0100

    Classes created to define direction, grid values and position

---
+++ SnakeGame/GridValue.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakeGame
+{
+    //A cell, in the grid, may be empty, contain part of the snake or be empty. 
+    public enum GridValue
+    {
+        Empty,
+        Snake,
+        Food,
+        Outside
+    }
+}
